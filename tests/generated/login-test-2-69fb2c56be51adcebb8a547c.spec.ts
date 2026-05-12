94213318784000d6d53f6c47c8241b674cae4d39
UJJWAL SHARMA <ujjwal.d2itechnology@gmail.com>
2026-05-12 16:32:50 +0530

    test(automation): update script for 69fb2c56be51adcebb8a547c

--- tests/generated/login-test-2-69fb2c56be51adcebb8a547c.spec.ts
+++ tests/generated/login-test-2-69fb2c56be51adcebb8a547c.spec.ts
@@ -4,6 +4,6 @@ test('test', async ({ page }) => {
   await page.goto('https://sandbox-crm.mobilecom.in/');
   await page.getByRole('button', { name: 'Get Started' }).nth(1).click();
   await page.getByRole('textbox', { name: 'Email address' }).click();
-  await page.getByRole('textbox', { name: 'Email address' }).fill('avasthi121.d2itechnology@gmail.com');
+  await page.getByRole('textbox', { name: 'Email address' }).fill('avasthi12133.d2itechnology@gmail.com');
   await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Pas...
